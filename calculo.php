7a091ac1ed90c7cff764a1473161b5e3a2818c3d
bioniki15 <86114297+bioniki15@users.noreply.github.com>
2022-10-04 11:56:44 -0300

    Update calculo.php

--- calculo.php
+++ calculo.php
@@ -3,7 +3,7 @@
     $posterior = $pNumero + 1;
     $antecessor = $pNumero - 1;
 
-    echo "O antecessor do número digitado é ", + $antecessor, "<br> E seu antecessor é ", +$posterior;
+    echo "O antecessor do número digitado é igual a  ", + $antecessor, "<br> E seu antecessor é ", +$posterior;
 
 
 ?>
